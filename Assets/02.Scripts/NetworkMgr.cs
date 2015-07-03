538de346f969ad308f991653858c6cc4393a4cf8
suda4 <blazblue666hazama@gmail.com>
2015-07-03 12:23:50 +0900

    ネットワークマネージャ作成

---
+++ Assets/02.Scripts/NetworkMgr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMgr : MonoBehaviour {
+
+	private const string ip = "127.0.0.1";
+
+	private const int port = 30000;
+
+	private bool _useNat = false;
+
+	void OnGUI()
+	{
+		if (Network.peerType == NetworkPeerType.Disconnected) {
+			if (GUI.Button (new Rect (20, 20, 200, 25), "Start Server")) {
+				Network.InitializeServer (20, port, _useNat);
+			}
+			if (GUI.Button (new Rect (20, 50, 200, 25), "Connect to Server")) {
+				Network.Connect (ip, port);
+			}
+		} else {
+			if (Network.peerType == NetworkPeerType.Server) {
+				GUI.Label (new Rect (20, 20, 200, 25), "Initialization Server...");
+			}
+			if (Network.peerType == NetworkPeerType.Client) {
+				GUI.Label (new Rect (20, 20, 200, 25), "Connected to Server");
+			}
+		}
+	}
+
+}
