f51de7be8f1cadb55cb13031a3493da64fe955ed
suda4 <blazblue666hazama@gmail.com>
2015-07-10 12:16:07 +0900

    network-mk

---
+++ Assets/02.Scripts/DisableNetworkUnlocalplayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableNetworkUnlocalplayerBehaviour : NetworkBehaviour {
+	public Behaviour cntrlScript;
+	public Camera camera;
+
+	void Start ()
+	{
+		if(! isLocalPlayer ){
+		
+			cntrlScript.enabled = false;
+			camera.enabled = false;
+		}
+	}
+	
+	void OnApplicationFocus(bool focusStatus) {
+		if(isLocalPlayer)
+		{
+			cntrlScript.enabled = focusStatus;
+			camera.enabled = true;
+			}
+	}
+}
+
